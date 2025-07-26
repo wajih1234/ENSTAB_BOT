151fc7fc5293e4af7f11069c26321a2fa84375e2
wajih1234 <74155158+wajih1234@users.noreply.github.com>
2025-07-26 03:17:56 +0200

    Update app.py

--- ENSTA_BOT1/app.py
+++ ENSTA_BOT1/app.py
@@ -9,7 +9,7 @@ import datetime
 app = Flask(__name__)
 CORS(app, resources={
     r"/predict": {
-        "origins": [https://enstab-bot.vercel.app","http://localhost:3000",
+        "origins": ["https://enstab-bot.vercel.app","http://localhost:3000",
             "http://localhost:5000", "http://127.0.0.1:5000"],
         "allow_headers": ["Authorization", "Content-Type"]
     }
